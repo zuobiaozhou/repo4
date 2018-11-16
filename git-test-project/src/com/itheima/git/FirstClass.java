e24bab8031daf7c323ab13f55aff21495428e536
zuobiaozhou <244454315@qq.com>
2018-11-16 21:21:26 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello 坐标轴");
+        System.out.println("Hello 坐标轴");
+        System.out.println("Hello 坐标轴");
+        System.out.println("Hello 坐标轴");
+        System.out.println("Hello 坐标轴");
+        System.out.println("Hello 坐标轴");
+    }
+}
