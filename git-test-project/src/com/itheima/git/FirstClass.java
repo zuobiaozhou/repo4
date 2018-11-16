82898490bf32976e5dfa885a308ea9d4d8167369
zuobiaozhou <244454315@qq.com>
2018-11-16 22:23:08 +0800

    修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -8,10 +8,12 @@ public class FirstClass {
         System.out.println("Hello 坐标轴");
         System.out.println("Hello 坐标轴");
         System.out.println("Hello 坐标轴");
+        //hello Test
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
